1ebd185fb721be8815154b2fb8dd1a6f59b23c7b
Miguel Dell'Uomini <delldor@gmail.com>
2016-01-31 14:05:53 -0430

    Actualizado aptitude en actunoporuno

--- debian.sh
+++ debian.sh
@@ -23,6 +23,28 @@ sudo sh -c "aptitude --visual-preview safe-upgrade; aptitude --visual-preview fu
 
 alias act='read -p "Actualizar todo el sistema parte por parte. Pulsa Enter" a; act0; act1; act2; act3'
 
+actunoporunosinconex(){
+read -p "Si hay que instalar algo, no se ejecuta. No descarga nuevos paquetes. Enter empieza" a
+sudo bash -c "aptitude search -F '%p' --disable-columns '~U'| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. |xargs -l1 apt-get install --no-install-recommends --no-download"
+paplay /usr/share/sounds/KDE-Im-Nudge.ogg
+}
+
+actunoporuno(){
+read -p	"Instala nuevos paquetes y busca descargar lo que falte. 
+Primero intenta con:
+sudo aptitude safe-upgrade
+
+Enter empieza" a
+
+sudo bash -c "apt-get upgrade -s |grep 'Inst '| cut -d' ' -f2| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^uno -e ^ure -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e python |xargs -l1 aptitude install --safe-resolver --allow-new-installs --allow-untrusted -y"
+
+sudo bash -c "aptitude search -F '%p' --disable-columns '~U'| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z]|xargs -l1 apt-get install"
+paplay /usr/share/sounds/KDE-Im-Nudge.ogg
+#apt-get install --no-install-recommends -y
+}
+
+
+
 it(){
 #Solo aptitude permite untrusted con opción directa
 echo "A instalar $@"
@@ -54,24 +76,4 @@ find "$1" -iname "*.deb" -exec sudo cp -vu {} /var/cache/apt/archives/ \;
 agregaclave(){
 echo -e "Procesando clave: $1"
 gpg --keyserver subkeys.pgp.net --recv $1 | gpg --keyserver  keyserver.ubuntu.com --recv $1 && gpg --export --armor $1 && sudo apt-key add -
-}
-
-actunoporunosinconex(){
-read -p "Si hay que instalar algo, no se ejecuta. No descarga nuevos paquetes. Enter empieza" a
-sudo bash -c "aptitude search -F '%p' --disable-columns '~U'| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. |xargs -l1 apt-get install --no-install-recommends --no-download"
-paplay /usr/share/sounds/KDE-Im-Nudge.ogg
-}
-
-actunoporuno(){
-read -p	"Instala nuevos paquetes y busca descargar lo que falte. 
-Primero intenta con:
-sudo aptitude safe-upgrade
-
-Enter empieza" a
-
-sudo bash -c "apt-get upgrade -s |grep 'Inst '| cut -d' ' -f2| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^uno -e ^ure -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. |xargs -l1 aptitude safe-upgrade --allow-new-installs --allow-untrusted -y"
-
-sudo bash -c "aptitude search -F '%p' --disable-columns '~U'| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z]|xargs -l1 apt-get install"
-paplay /usr/share/sounds/KDE-Im-Nudge.ogg
-#apt-get install --no-install-recommends -y
 }
