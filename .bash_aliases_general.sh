e334ee593c4129ef5febb086f4d0fd1174788657
Miguel Dell'Uomini <delldor@gmail.com>
2016-02-05 13:32:51 -0430

    Update .bash_aliases_general.sh

--- .bash_aliases_general.sh
+++ .bash_aliases_general.sh
@@ -9,7 +9,6 @@ set LC_MESSAGES="es"
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-
 echo "==================
 Arrancando .bashrc"
 
