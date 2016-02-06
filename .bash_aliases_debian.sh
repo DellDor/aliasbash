656a6fc7e7479737869ec31da87c82ab58fe2bd8
Miguel Dell'Uomini <delldor@gmail.com>
2016-02-06 10:00:45 -0430

    Update .bash_aliases_debian.sh

--- .bash_aliases_debian.sh
+++ .bash_aliases_debian.sh
@@ -36,7 +36,8 @@ echo "################ #################### ########################
 Actualiza paquetes importantes y el resto uno por uno."
 
 sudo bash -c "grep -h '^deb.*security' /etc/apt/sources.list /etc/apt/sources.list.d/* >/tmp/borrame && aptitude safe-upgrade -o Dir::Etc::SourceList=/tmp/borrame -o Dir::Etc::sourceparts=/nonexistingdir && aptitude search '?or(~pstandard, ~pimportant, ~prequired, ~E) ~U' -F %p |xargs aptitude safe-upgrade  && \
-echo "#############Sigue todo sin bibliotecas ni eliminación#############" && apt-get upgrade -s |grep 'Inst '| cut -d' ' -f2| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^uno -e ^ure -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e python |xargs -l1 aptitude install --safe-resolver --allow-new-installs --allow-untrusted -y"
+echo "#############Sigue todo sin bibliotecas ni eliminación#############" && apt-get upgrade -s |grep 'Inst '| cut -d' ' -f2| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z] -e ^uno -e ^ure -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e python |xargs -l1 apt-get install --no-remove -y"
+##aptitude install --safe-resolver --allow-new-installs --allow-untrusted -y
 read -p "Enter para continuar con posibilidad de preguntar si borrar algún paquete" a
 sudo bash -c "aptitude search -F '%p' --disable-columns '~U'| grep -v -e ^lib[a-q] -e ^lib[s-z] -e ^wine -e python -e plasma -e ruby -e ^glib -e common -e data -e ^gir1. -e ^libr[a-d] -e ^libr[f-z] -e ^libre[a-n] -e ^libre[p-z]|xargs -l1 apt-get install"
 #paplay /usr/share/sounds/KDE-Im-Nudge.ogg
